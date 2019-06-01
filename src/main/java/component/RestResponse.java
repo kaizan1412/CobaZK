63559fbd16eab1d8945dc60e03b1ef61d179248e
kaizan1412 <urameshi.yusuke75@gmail.com>
2019-06-01 17:46:54 +0700

    comit all

---
+++ src/main/java/component/RestResponse.java
@@ -0,0 +1,43 @@
+package component;
+
+public class RestResponse {
+	
+	private int responseCode;
+	private String message;
+	private Object obj;
+	
+	public RestResponse() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	public RestResponse(int responseCode, String message, Object obj) {
+		super();
+		this.responseCode = responseCode;
+		this.message = message;
+		this.obj = obj;
+	}
+
+
+	public int getResponseCode() {
+		return responseCode;
+	}
+	public void setResponseCode(int responseCode) {
+		this.responseCode = responseCode;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public Object getObj() {
+		return obj;
+	}
+	public void setObj(Object obj) {
+		this.obj = obj;
+	}
+	
+	
+
+}
--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -8,10 +8,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClientException;
 
+import component.RestResponse;
 import dto.MstUserDto;
 import service.MstUserService;
 
@@ -26,14 +30,60 @@ public class UserController {
 			value="/all",
 			method=RequestMethod.GET,
 			produces=MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity findAll(){
+	public RestResponse findAll(){
 		try {
-			return new ResponseEntity<List<MstUserDto>>(mstUserSvc.findAll(), null, HttpStatus.OK);
-//			return mstUserSvc.findAll();
-		} catch (Exception e) {
+			
+			if(mstUserSvc.findAll() != null && mstUserSvc.findAll().size() > 0){
+				return new RestResponse(HttpStatus.OK.value(), "Success", mstUserSvc.findAll());
+			}
+			
+		} catch (RestClientException e) {
 			e.printStackTrace();
-			return null;
+			return new RestResponse(HttpStatus.NOT_FOUND.value(), e.getLocalizedMessage(), null);
 		}
+		return null;
+	}
+	
+	@RequestMapping(value="/findByUsernamePassword/{username}/{password}", 
+				method=RequestMethod.GET,
+				produces=MediaType.APPLICATION_JSON_VALUE)
+	public RestResponse findByUsernamePassword(
+				@PathVariable("username") String username, 
+				@PathVariable("password") String password){
+		
+		try {
+			
+			if(mstUserSvc.findByUsernamePassword(username, password) != null 
+					&& !mstUserSvc.findByUsernamePassword(username, password).getUsername().equalsIgnoreCase("")){
+				return new RestResponse(HttpStatus.OK.value(), "Success", mstUserSvc.findByUsernamePassword(username, password));
+			}
+			
+		} catch (RestClientException e) {
+			e.printStackTrace();
+			return new RestResponse(HttpStatus.NOT_FOUND.value(), e.getLocalizedMessage(), null);
+		}
+		return null;
+	}
+	
+	@RequestMapping(value="/findByUsernamePassword2", 
+			method=RequestMethod.GET,
+			produces=MediaType.APPLICATION_JSON_VALUE)
+	public RestResponse findByUsernamePassword2(
+				@RequestParam("username") String username, 
+				@RequestParam("password") String password){
+		
+		try {
+			
+			if(mstUserSvc.findByUsernamePassword(username, password) != null 
+					&& !mstUserSvc.findByUsernamePassword(username, password).getUsername().equalsIgnoreCase("")){
+				return new RestResponse(HttpStatus.OK.value(), "Success", mstUserSvc.findByUsernamePassword(username, password));
+			}
+			
+		} catch (RestClientException e) {
+			e.printStackTrace();
+			return new RestResponse(HttpStatus.NOT_FOUND.value(), e.getLocalizedMessage(), null);
+		}
+		return null;
 	}
 
 }
